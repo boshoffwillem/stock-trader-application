f8af1c19675ba45e075d031b7340b135dc927b2c
Willem Boshoff <boshoffwillem@protonmail.com>
2020-11-19 11:24:25 +0200

    Set up database

---
+++ TradingApplication/TradingApplication.Domain/Models/AssetTransaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingApplication.Domain.Models
+{
+    public class AssetTransaction
+    {
+        public int Id { get; set; }
+
+        public Account Account { get; set; }
+
+        public bool IsPurchased { get; set; }
+
+        public Stock Stock { get; set; }
+
+        public int Shares { get; set; }
+
+        public DateTime DateProcessed { get; set; }
+    }
+}
