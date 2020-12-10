b793a24053e6f8ff47c9bc8f46ff20cf59b410c3
Willem Boshoff <boshoffwillem@protonmail.com>
2020-12-10 17:20:04 +0200

    Created base WPF project with MVVM framework.

---
+++ TradingApplication/TradingApplication.WPF/State/Navigators/INavigator.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using TradingApplication.WPF.ViewModels;
+
+namespace TradingApplication.WPF.State.Navigators
+{
+   public enum ViewType
+   {
+      Home,
+      Portfolio
+   }
+
+   public interface INavigator
+   {
+      BaseViewModel CurrentViewModel { get; set; }
+
+      ICommand UpdateCurrentViewModelCommand { get; }
+   }
+}
---
+++ TradingApplication/TradingApplication.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TradingApplication.WPF.State.Navigators;
+
+namespace TradingApplication.WPF.ViewModels
+{
+   public class MainViewModel : BaseViewModel
+   {
+      public INavigator Navigator { get; set; } = new Navigator();
+   }
+}
